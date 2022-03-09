3e09437a7315f0ec4b5574d30bf774792a67d472
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2022-03-09 23:44:31 +0530

    updated game page

--- frontend/src/components/Game/GameList.js
+++ frontend/src/components/Game/GameList.js
@@ -1,22 +1,42 @@
 const gameList = {
     0: [
-        { question: "", image: null, isAdult: false },
+        { question: "1st question 0", image: null, isAdult: false },
+        { question: "2nd question 0", image: null, isAdult: false },
+        { question: "3rd question 0", image: null, isAdult: false },
+        { question: "4th question 0", image: null, isAdult: false },
+        { question: "5th question 0", image: null, isAdult: false },
     ],
 
     1: [
-        { question: "", image: null, isAdult: false },
+        { question: "1st question 1", image: null, isAdult: false },
+        { question: "2nd question 1", image: null, isAdult: false },
+        { question: "3rd question 1", image: null, isAdult: false },
+        { question: "4th question 1", image: null, isAdult: false },
+        { question: "5th question 1", image: null, isAdult: false },
     ],
 
     2: [
-        { question: "", image: null, isAdult: false },
+        { question: "1st question 2", image: null, isAdult: false },
+        { question: "2nd question 2", image: null, isAdult: false },
+        { question: "3rd question 2", image: null, isAdult: false },
+        { question: "4th question 2", image: null, isAdult: false },
+        { question: "5th question 2", image: null, isAdult: false },
     ],
 
     3: [
-        { question: "", image: null, isAdult: false },
+        { question: "1st question 3", image: null, isAdult: false },
+        { question: "2nd question 3", image: null, isAdult: false },
+        { question: "3rd question 3", image: null, isAdult: false },
+        { question: "4th question 3", image: null, isAdult: false },
+        { question: "5th question 3", image: null, isAdult: false },
     ],
 
     4: [
-        { question: "", image: null, isAdult: false },
+        { question: "1st question 4", image: null, isAdult: false },
+        { question: "2nd question 4", image: null, isAdult: false },
+        { question: "3rd question 4", image: null, isAdult: false },
+        { question: "4th question 4", image: null, isAdult: false },
+        { question: "5th question 4", image: null, isAdult: false },
     ],
 
     5: [
@@ -24,11 +44,19 @@ const gameList = {
     ],
 
     6: [
-        { question: "", image: null, isAdult: false },
+        { question: "1st question 6", image: null, isAdult: false },
+        { question: "2nd question 6", image: null, isAdult: false },
+        { question: "3rd question 6", image: null, isAdult: false },
+        { question: "4th question 6", image: null, isAdult: false },
+        { question: "5th question 6", image: null, isAdult: false },
     ],
 
     7: [
-        { question: "", image: null, isAdult: false },
+        { question: "1st question 7", image: null, isAdult: false },
+        { question: "2nd question 7", image: null, isAdult: false },
+        { question: "3rd question 7", image: null, isAdult: false },
+        { question: "4th question 7", image: null, isAdult: false },
+        { question: "5th question 7", image: null, isAdult: false },
     ],
 
     8: [
--- routes/api.js
+++ routes/api.js
@@ -2,21 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Game = require('../models/game');
 
-router.get('/game', (req, res, next) => {
-    // This will return all the data, exposing only the id and action field to the client
-    Game.find()
-        .then((data) => res.json(data))
-        .catch(next);
-});
-
 router.post('/game', (req, res, next) => {
     Game.create(req.body)
         .then((data) => res.json(data))
         .catch(next);
 });
 
-router.delete('/game/:id', (req, res, next) => {
-    Game.findOneAndDelete({ _id: req.params.id })
+router.get('/game/:slug', (req, res, next) => {
+    Game.findOne({ slug: req.params.slug })
         .then((data) => res.json(data))
         .catch(next);
 });
